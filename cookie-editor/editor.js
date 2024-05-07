f2a5b00496e3fb4c7c308883b56bf4d5e7d41e89
Zsofia Szatmari <zsofi25szatmari@gmail.com>
2024-05-07 13:37:19 +0200

    copy to clipboard

--- cookie-editor/editor.js
+++ cookie-editor/editor.js
@@ -2,9 +2,24 @@ const copyButton = document.getElementById('copy-button');
 const form = document.getElementById('cookie-form');
 const messages = document.getElementById('messages');
 
-copyButton.addEventListener('click', () => {
+copyButton.addEventListener('click', async () => {
   const [[,name]] = new FormData(form).entries();
-  console.log('set to copy', name);
+  const [tab] = await chrome.tabs.query({active: true, currentWindow: true});
+  
+  chrome.cookies.get({
+    name,
+    url: tab.url
+  }).then((cookie) => {
+    if (cookie) {
+      navigator.clipboard.writeText(cookie.value);
+      setMessage('Copied to clipboard');
+    } else {
+      setMessage('Cookie not found');
+    }
+  }).catch((e) => {
+    setMessage('Failure :( Check logs for error');
+    console.log('Cookie copy error', e);
+  })
 });
 
 form.addEventListener('submit', async (event) => {
