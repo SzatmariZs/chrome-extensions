8c8fc0b6e9e92d544bfd4019111cb3412cd7b3b3
Zsofia Szatmari <zsofi25szatmari@gmail.com>
2024-05-07 17:31:29 +0200

    with remove button, not really working

--- cookie-editor/editor.js
+++ cookie-editor/editor.js
@@ -1,14 +1,17 @@
 const copyButton = document.getElementById('copy-button');
+const removeButton = document.getElementById('remove-button');
 const form = document.getElementById('cookie-form');
 const messages = document.getElementById('messages');
 
 copyButton.addEventListener('click', async () => {
-  const [[,name]] = new FormData(form).entries();
+  const { id: name } = getFormValues();
   const [tab] = await chrome.tabs.query({active: true, currentWindow: true});
+
+  clearMessages();
   
   chrome.cookies.get({
     name,
-    url: tab.url
+    url: new URL(tab.url).origin
   }).then((cookie) => {
     if (cookie) {
       navigator.clipboard.writeText(cookie.value);
@@ -18,7 +21,7 @@ copyButton.addEventListener('click', async () => {
     }
   }).catch((e) => {
     setMessage('Failure :( Check logs for error');
-    console.log('Cookie copy error', e);
+    console.log('Cookie copy error:', e);
   })
 });
 
@@ -27,14 +30,31 @@ form.addEventListener('submit', async (event) => {
 
   clearMessages();
 
-  const [ [,name], [,value] ] = new FormData(event.target).entries();
+  const { id: name, value } = getFormValues();
   const [tab] = await chrome.tabs.query({active: true, currentWindow: true});
 
   chrome.cookies.set({
     name,
     value,
-    url: tab.url
+    url: new URL(tab.url).origin
   }, (cookie) => cookie ? setMessage('Cookie is set!') : setMessage('Failure :('))
+});
+
+removeButton.addEventListener('click', async () => {
+  const { id: name } = getFormValues();
+  const [tab] = await chrome.tabs.query({active: true, currentWindow: true});
+
+  clearMessages();
+
+  chrome.cookies.remove({
+    name,
+    url: new URL(tab.url).origin
+  }).then(() => {
+    setMessage('Cookie removed successfully');
+  }).catch((e) => {
+    setMessage('Failure :( Check logs for error');
+    console.log('Cookie removal error:', e);
+  })
 });
 
 function clearMessages() {
@@ -43,6 +63,10 @@ function clearMessages() {
 
 function setMessage(m) {
   messages.textContent = m;
+}
+
+function getFormValues() {
+  return [...(new FormData(form).entries())].reduce((acc, [key, value]) => ({ ...acc, [key]: value }), {});
 }
 
 
